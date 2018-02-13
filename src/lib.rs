e8f8573a393fbeadfd2b9de338a5bee1eac17500
M Farkas-Dyck <strake888@gmail.com>
2018-02-12 18:05:19 -0800

    storage

--- src/lib.rs
+++ src/lib.rs
@@ -4,34 +4,24 @@ extern crate siphasher as sip;
 
 use core::borrow::Borrow;
 use core::hash::{Hash, Hasher};
-use core::marker::PhantomData;
-use core::{mem, ptr, slice};
+use core::ops::{Index, IndexMut, RangeFull};
+use core::{mem, ptr};
 
 #[derive(Clone)]
 pub struct DefaultHasher(::sip::sip::SipHasher);
 
-pub struct RefMut<'a, K: 'a + Eq + Hash, T: 'a, H: Clone + Hasher = DefaultHasher> {
-    φ: PhantomData<&'a [(K, T)]>,
-    log_cap: u32,
-    hash_ptr: *mut usize,
-    elem_ptr: *mut (K, T),
+pub struct HashTable<K: Eq + Hash, T,
+                     Hs: IndexMut<usize, Output = usize> + Index<RangeFull, Output = [usize]>,
+                     Es: IndexMut<usize, Output = (K, T)>, H: Clone + Hasher = DefaultHasher> {
+    hashes: Hs,
+    elems : Es,
     free_n: usize,
     hasher: H,
 }
 
-impl<'a, K: Eq + Hash, T, H: Clone + Hasher> RefMut<'a, K, T, H> {
-    fn components_mut(&mut self) -> (&mut [usize], &mut [(K, T)]) {
-        let cap = 1<<self.log_cap;
-        unsafe {
-            (slice::from_raw_parts_mut(self.hash_ptr, cap),
-             slice::from_raw_parts_mut(self.elem_ptr, cap))
-        }
-    }
-
-    fn components(&self) -> (&[usize], &[(K, T)]) { unsafe {
-        let (hashes, elems) = (self as *const Self as *mut Self).as_mut().unwrap().components_mut();
-        (hashes, elems)
-    } }
+impl<K: Eq + Hash, T, Hs: IndexMut<usize, Output = usize> + Index<RangeFull, Output = [usize]>,
+     Es: IndexMut<usize, Output = (K, T)>, H: Clone + Hasher> HashTable<K, T, Hs, Es, H> {
+    fn log_cap(&self) -> u32 { 0usize.count_zeros() - self.hashes[..].len().leading_zeros() - 1 }
 
     fn hash<Q: ?Sized>(&self, k: &Q) -> usize where Q: Hash {
         let mut h = self.hasher.clone();
@@ -41,14 +31,13 @@ impl<'a, K: Eq + Hash, T, H: Clone + Hasher> RefMut<'a, K, T, H> {
 
     fn find_ix<Q: ?Sized>(&self, k: &Q) -> Option<usize> where K: Borrow<Q>, Q: Eq + Hash {
         debug_assert!(self.free_n >= 1);
-        let wrap_mask = (1<<self.log_cap)-1;
+        let wrap_mask = (1 << self.log_cap()) - 1;
         let h = self.hash(k);
         let mut i = h & wrap_mask;
         let mut psl = 0;
-        let (hashes, elms) = self.components();
         loop {
-            if hashes[i] == 0 || psl > compute_psl(hashes, i) { return None };
-            if hashes[i] == h && elms[i].0.borrow() == k { return Some(i); }
+            if self.hashes[i] == 0 || psl > compute_psl(&self.hashes[..], i) { return None };
+            if self.hashes[i] == h && self.elems[i].0.borrow() == k { return Some(i); }
             i = (i+1)&wrap_mask;
             debug_assert_ne!(h & wrap_mask, i);
             psl += 1;
@@ -57,12 +46,12 @@ impl<'a, K: Eq + Hash, T, H: Clone + Hasher> RefMut<'a, K, T, H> {
 
     #[inline]
     pub fn find_with_ix<Q: ?Sized>(&self, k: &Q) -> Option<(usize, &K, &T)> where K: Borrow<Q>, Q: Eq + Hash {
-        self.find_ix(k).map(move |i| { let (_, elms) = self.components(); (i, &elms[i].0, &elms[i].1) })
+        self.find_ix(k).map(move |i| (i, &self.elems[i].0, &self.elems[i].1))
     }
 
     #[inline]
     pub fn find_mut_with_ix<Q: ?Sized>(&mut self, k: &Q) -> Option<(usize, &K, &mut T)> where K: Borrow<Q>, Q: Eq + Hash {
-        self.find_ix(k).map(move |i| { let (_, elms) = self.components_mut(); (i, &elms[i].0, &mut elms[i].1) })
+        self.find_ix(k).map(move |i| { let &mut (ref k, ref mut v) = &mut self.elems[i]; (i, k, v) })
     }
 
     #[inline]
@@ -76,40 +65,42 @@ impl<'a, K: Eq + Hash, T, H: Clone + Hasher> RefMut<'a, K, T, H> {
     }
 
     #[inline]
-    pub fn insert_with<F: FnOnce(Option<T>) -> T>(&mut self, k: K, f: F) -> Result<(usize, &mut K, &mut T), (K, F)> {
+    pub fn insert_with<F: FnOnce(Option<T>) -> T>(&mut self, k: K, f: F) -> Result<(usize, &K, &mut T), (K, F)> {
         if 1 >= self.free_n { return Err((k, f)); }
         self.free_n -= 1;
 
-        let cap = 1<<self.log_cap;
+        let cap = 1 << self.log_cap();
         let mut h = self.hash(&k);
         let mut i = h&(cap-1);
         let mut psl = 0;
-        let (hashes, elms) = self.components_mut();
         loop {
-            if hashes[i] == 0 {
-                hashes[i] = h;
-                unsafe { ptr::write(&mut elms[i], (k, f(None))); }
-                return Ok((i, &mut elms[i].0, &mut elms[i].1))
+            if self.hashes[i] == 0 {
+                self.hashes[i] = h;
+                unsafe { ptr::write(&mut self.elems[i], (k, f(None))); }
+                let &mut (ref k, ref mut v) = &mut self.elems[i];
+                return Ok((i, k, v))
             }
 
-            if hashes[i] == h && elms[i].0 == k {
+            if self.hashes[i] == h && self.elems[i].0 == k {
                 unsafe {
-                    hashes[i] |=  dead_flag;
-                    let x = ptr::read(&elms[i].1);
-                    ptr::write(&mut elms[i].1, f(Some(x)));
-                    hashes[i] &= !dead_flag;
+                    self.hashes[i] |=  dead_flag;
+                    let x = ptr::read(&self.elems[i].1);
+                    ptr::write(&mut self.elems[i].1, f(Some(x)));
+                    self.hashes[i] &= !dead_flag;
                 }
-                return Ok((i, &mut elms[i].0, &mut elms[i].1))
+                let &mut (ref k, ref mut v) = &mut self.elems[i];
+                return Ok((i, k, v))
             }
 
-            if psl > compute_psl(hashes, i) {
+            if psl > compute_psl(&self.hashes[..], i) {
                 let mut e = (k, f(None));
                 loop {
-                    mem::swap(&mut h, &mut hashes[i]);
-                    mem::swap(&mut e, &mut elms[i]);
+                    mem::swap(&mut h, &mut self.hashes[i]);
+                    mem::swap(&mut e, &mut self.elems[i]);
                     if h == 0 || is_dead(h) {
                         mem::forget(e);
-                        return Ok((i, &mut elms[i].0, &mut elms[i].1));
+                        let &mut (ref k, ref mut v) = &mut self.elems[i];
+                        return Ok((i, k, v));
                     };
                     i = (i+1)&(cap-1);
                 }
@@ -137,10 +128,9 @@ impl<'a, K: Eq + Hash, T, H: Clone + Hasher> RefMut<'a, K, T, H> {
     pub fn delete<Q: ?Sized>(&mut self, k: &Q) -> Option<T> where K: Borrow<Q>, Q: Eq + Hash {
         self.find_ix(k).map(move |i| unsafe {
             self.free_n += 1;
-            debug_assert!(1 << self.log_cap >= self.free_n);
-            let (hashes, elms) = self.components_mut();
-            let (_, x) = ptr::read(&elms[i]);
-            hashes[i] |= dead_flag;
+            debug_assert!(1 << self.log_cap() >= self.free_n);
+            let (_, x) = ptr::read(&self.elems[i]);
+            self.hashes[i] |= dead_flag;
             x
         })
     }
